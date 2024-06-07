032113990b0574b47c701c3de026712cd2a778ef
naaadang <naaadang@naver.com>
2024-06-07 20:05:12 +0900

    complete

--- config/settings.py
+++ config/settings.py
@@ -153,4 +153,5 @@ CORS_ALLOW_ALL_ORIGINS=True
 CSRF_TRUSTED_ORIGINS=[
     'http://localhost:3000',
     'http://127.0.0.1:8000',
+    'https://port-0-filmcritique-server-1mrfs72llwumekr6.sel5.cloudtype.app'
 ]
