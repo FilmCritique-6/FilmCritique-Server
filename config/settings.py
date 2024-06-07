61dc84dc94c918afcdb3abe64a690962b53bc16f
naaadang <naaadang@naver.com>
2024-06-07 19:51:23 +0900

    password 인증 구현

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY=os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,9 +46,13 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'critique',
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +143,14 @@ MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 # static 루트 설정
 STATIC_URL='/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,"static")
+
+
+#cors 설정
+CORS_ALLOWED_CREDENTIALS=True
+CORS_ALLOW_ALL_ORIGINS=True
+
+#csrf 설정
+CSRF_TRUSTED_ORIGINS=[
+    'http://localhost:3000',
+    'http://127.0.0.1:8000',
+]
--- critique/views.py
+++ critique/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .serializers import ReviewSerializer
 from .models import Review
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
 
 
@@ -9,4 +12,14 @@ class ReviewViewset(ModelViewSet):
     queryset=Review.objects.all()
     serializer_class=ReviewSerializer
 
+
+
+@api_view(['POST'])
+def validate_password(request,pk):
+    review=get_object_or_404(Review,pk=pk)
+    password=request.data['password']
+
+    if password==review.password:
+        return Response({"password":True})
     
+    return Response({"password":False})
